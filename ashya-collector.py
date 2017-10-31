0ef6d4622593294066c60157f6607e790474940b
vallard <vallard@benincosa.com>
2017-10-31 08:50:59 -0700

    updated in new function.

--- ashya-collector.py
+++ ashya-collector.py
@@ -13,16 +13,21 @@ app = Flask(__name__)
 @app.route("/urls", )
 def default():
         return jsonF.payload
-    
-if __name__ == '__main__': 
-    app.run(port=5050, debug = True)
+   
+def send_data_loop():
     while True:
         print("sending data")
         payload = open("urls.json").read()
         urls = json.loads(payload)
         for link in jsonF.urls["urls"]:
             response = requests.post(url=link, data=jsonF.payload)
-            print(response)
+            print(response.json())
         time.sleep(5)
+
+
+ 
+if __name__ == '__main__': 
+    #app.run(port=5050, debug = True)
+    send_data_loop()
     
         
