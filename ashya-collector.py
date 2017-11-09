cfcdfd3e5dac228b028935a07f2ec01b85bf1311
vallard <vallard@benincosa.com>
2017-11-09 11:15:27 -0500

    moved to main collector.

--- ashya-collector.py
+++ ashya-collector.py
@@ -1,33 +1,38 @@
 from flask import Flask
 import requests
 import json
+import threading
 import time
 
 class JsonF():
-    payload = open("urls.json").read()
-    urls = json.loads(payload)
+     payload = open("urls.json").read()
+     urls = json.loads(payload)
 
 jsonF = JsonF()    
 app = Flask(__name__)
    
-@app.route("/urls", )
+@app.route("/urls", methods=['GET'])
 def default():
-        return jsonF.payload
-   
-def send_data_loop():
-    while True:
-        print("sending data")
-        payload = open("urls.json").read()
-        urls = json.loads(payload)
+        return (jsonF.payload)
+            
+def background():
+ 
         for link in jsonF.urls["urls"]:
             response = requests.post(url=link, data=jsonF.payload)
             print(response.json())
+            print (link) 
+            print("data sent inside for loop")
+
+def do_stuff():
+    while True:  
+        background()
         time.sleep(5)
-
-
- 
-if __name__ == '__main__': 
-    #app.run(port=5050, debug = True)
-    send_data_loop()
     
-        
+_thread = threading.Thread(target=do_stuff)
+_thread.setDaemon(True)
+_thread.start()
+
+if __name__ == '__main__': 
+    app.run(port=5050, debug = True)
+    default()
+    do_stuff()
