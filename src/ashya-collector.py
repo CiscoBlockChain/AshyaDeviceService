6c66a2363306292c96c7a8fc55fbb0773cb834cd
vallard <vallard@benincosa.com>
2018-06-20 08:30:15 -0700

    added some scaffold.

--- src/ashya-collector.py
+++ src/ashya-collector.py
@@ -15,9 +15,17 @@ CORS(app)
 @cross_origin()
 def default():
         return (jsonF.payload)
-        
-    
-        
+
+
+@app.route("/contract", methods=['POST', 'GET'])
+@cross_origin()
+def contract():
+    if (method == "POST"):
+        # takes in argument of a contract { "contract" : "0x4ffff..." }
+        write_contract()       
+    elif(method == "GET"):
+        return contract_address
+
 def background():
  
         for link in jsonF.urls["urls"]:
@@ -26,8 +34,15 @@ def background():
             print (link) 
             print("data sent inside for loop")
             
-
-   
+def write_contract(json_data): 
+    file_name = "/etc/ashya/device_contract.json"   
+    with open(file_name, "w") as f:
+        f.write(json_data.dump)
+    
+def read_contract():
+    file_name = "/etc/ashya/device_contract.json"   
+    with open(file_name, "r") as f: 
+        
 
 def do_stuff():
     while True:  
