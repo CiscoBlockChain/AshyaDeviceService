be6d5c9f04e05f97ca35d152bed127f6c62aef88
sajustin <sajustin@cisco.com>
2018-07-09 14:02:10 +0200

    completer read/write functions

--- src/DeviceCollector.py
+++ src/DeviceCollector.py
@@ -6,32 +6,47 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)   
 
-class DeviceColletor():  
-    data = []
+class DeviceColletor():
+    addresses = []
+
+    def write_contract(json_data):
+        file_name = "etc/ashya/device_contract.json"
+        with open(file_name, 'w+') as outfile:
+            json.dump(json_data, outfile)
+
+    def read_contract():
+        file_name = "etc/ashya/device_contract.json"
+        try:
+            with open(file_name, "r") as f:
+                return json.load(f)
+        except:
+            return {'address': []}
 
     def main():
         web3 = Web3(HTTPProvider('https://kovan.infura.io/'))
-        print (web3, "web3")
+        print(web3, "web3")
+        ## Hardcoded ashyaRegistry address
         address = Web3.toChecksumAddress('0xd32c3dd15f59490e49ffd7d5da2373808e4f9d21')
         contract = web3.eth.contract(address=address, abi=AshyaRegistry_ABI.abi)
-    
+        newAddresses = {}
+
         nb = contract.functions.getItemCount().call()
-        for i in range(0,nb):
+        print(nb)
+        for i in range(0, nb):
             temp = contract.functions.getItemAtIndex(i).call()
-            if DeviceColletor.data.count(temp) == 0:
-                DeviceColletor.data.append(temp)
-        DeviceColletor.write_contract(DeviceColletor.data,"/etc/ashya/device_contract.json") 
-        DeviceColletor.read_contract()
-        
-    def write_contract(json_data, fileName): 
-        file_name = fileName 
-        with open(file_name, 'w') as outfile:
-            json.dump(json_data, outfile)
-            
-    def read_contract():
-        file_name = "/etc/ashya/device_contract.json"   
-        with open(file_name, "r") as f:         
-               print(json.load(f))     
+            if DeviceColletor.addresses.count(temp) == 0:
+                DeviceColletor.addresses.append(temp)
+        print(DeviceColletor.addresses)
+
+        newAddresses['address'] = DeviceColletor.addresses
+        addresses = DeviceColletor.read_contract()
+        for a in DeviceColletor.addresses:
+            print(a)
+            print(addresses)
+            if (a not in addresses['address']):
+                addresses['address'].append(a)
+
+        DeviceColletor.write_contract(addresses)
 
 @app.route("/urls", methods=['GET'])
 @cross_origin()      
