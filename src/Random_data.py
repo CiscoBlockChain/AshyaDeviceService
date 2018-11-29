2d3d59c19d72acc2f6f47c619dbc579319f23d4c
sanaAB <sanaabohelal@gmail.com>
2018-11-29 17:28:55 +0100

    Update Random_data.py

--- src/Random_data.py
+++ src/Random_data.py
@@ -1,32 +1,92 @@
 from __future__ import absolute_import
-from faker import Faker
-from kafka import KafkaProducer, KafkaConsumer
+from web3 import Web3,HTTPProvider
+import  device_ABI
+import json
+import requests
+from flask import Flask, request, jsonify
+import threading, time
+from flask_cors import CORS, cross_origin
+from kafka import KafkaConsumer
 
-def kafka_connect():
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    temp = random_data()
-    print("here")
-    try:
-        #producer = KafkaProducer(value_serializer=lambda v: str.encode('utf-8'))
-        producer = KafkaProducer(key_serializer=str.encode)
-        producer.send('test', key='my message', value=str.encode(temp))
-        #producer.send('test',temp)
-        producer.flush()
-        print('Message published successfully.')
-    except Exception as ex:
-        print('Exception in publishing message')
-        print(str(ex))
-        
-#def kafka_get_values():
-#    consumer = KafkaConsumer('test')
-#    for msg in consumer:
-#        print(msg)  
+app = Flask(__name__)
+CORS(app)  
+contract_file = "C:/CiscoBlockchain/web-service/src/etc/ashya/device_contract.json"
 
-def random_data():
-    fake = Faker()
-    for _ in range(3):
-        return fake.text()
-        
-        
+@app.route("/contract", methods=['POST', 'GET'])
+@cross_origin()
+def contract():
+    if (request.method == "POST"):
+        return write_contract(request.json, contract_file)
+    elif(request.method == "GET"):
+        return jsonify(read_contract(contract_file))
+  
+def write_contract(json_data, file_name):
+    print("json data: ", json_data)
+    with app.test_request_context():
+        with open(file_name, 'w+') as outfile:
+            json.dump(json_data, outfile)  
+        return jsonify(read_contract(file_name)), 201
+
+def read_contract(file_name):
+   try:
+       with open(file_name, "r") as f:
+           return json.load(f)
+   except Exception as ex:
+           print(ex)
+           return {'address': ""}
+       
+@app.route("/urls", methods=['GET'])
+@cross_origin()      
+def get_urls():
+    urls = collect_urls()    
+    if urls:
+        return jsonify({"urls" : urls}), 200
+    return jsonify({"urls":[]}), 200
+
+def collect_urls():
+    urls = []
+    contractHash = read_contract(contract_file)
+    print(contractHash)
+    if not 'address' in contractHash:
+        return urls
+    address = contractHash["address"]
+    print("address: {0}".format(address))
+    if address:   
+        web3 = Web3(HTTPProvider('https://kovan.infura.io/'))
+        contract = web3.eth.contract(address= address, abi = device_ABI.abi)
+        nb = contract.functions.getURLCount().call()
+        print(nb)
+        for i in range(0,nb):
+            urls = contract.functions.urls(i).call()
+            payload = kafka_consumer()
+            requests.post(urls(i), data= payload)
+        print(urls)  
+    return urls
+            
+#retriving data from kafka service and send them to DeviceUrl        
+def kafka_consumer():
+    values = []
+    print(" insude consumer")
+    consumer = KafkaConsumer('test')
+    for message in consumer:
+        print(message.value)
+        values.append(message.value)       
+ 
+def do_stuff():
+    with app.test_request_context():
+        while True:  
+            collect_urls()
+            kafka_consumer()
+            time.sleep(15)  
+      
+with app.test_request_context():           
+    _thread = threading.Thread(target=do_stuff)
+    _thread.setDaemon(True)
+    _thread.start()
+    
+
 if __name__ == "__main__":
-    kafka_connect()
+    print ("Script has started...")
+    app.run(debug = True,host = '0.0.0.0',port=5050)
+    do_stuff()
+    print ("Script has ended...")
